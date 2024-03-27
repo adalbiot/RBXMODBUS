75d6854bac49e0b3e4de7015682552992b40e7a0
adalbiot <adalbiot@gmail.com>
2024-03-27 19:19:15 -0300

    a

--- index.js
+++ index.js
@@ -14,43 +14,25 @@ app.use(cors());
 app.post("/", jsonParser, function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
   bit0 = req.body.up;
-  console.log(bit0);
-  res.end();
   bit1 = req.body.down;
-  console.log(bit1);
-  res.end();
   bit2 = req.body.right;
-  console.log(bit2);
-  res.end();
   bit3 = req.body.left;
-  console.log(bit3);
-  res.end();
   bit4 = req.body.garra;
-  console.log(bit4);
-  res.end();
-
 });
+  console.log(bit0, bit1, bit2, bit3, bit4)
 
 app.get("/", function (req, res) {
   res.writeHead(200, { "Content-Type": "application/json", mode: "cors" });
   res.write(
     JSON.stringify({
       up: bit0,
-    }),
-    JSON.stringify({
       down: bit1,
-    }),
-    JSON.stringify({
       right: bit2,
-    }),
-    JSON.stringify({
       left: bit3,
-    }),
-    JSON.stringify({
-      garra: bit4,
-    }),
+      garra: bit4
+    })
   );
   res.end();
 });
 
-app.listen(3000);
+app.listen(3001);
